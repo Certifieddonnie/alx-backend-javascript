3db91530140063440b1ca893cbb965cd8d4df274
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-04-19 22:08:39 +0100

    [Task 0 - 5]: NodeJs

---
+++ 0x01-ES6_promise/.eslintrc.js
@@ -0,0 +1,40 @@
+module.exports = {
+    env: {
+      browser: false,
+      es6: true,
+      jest: true,
+    },
+    extends: [
+      'airbnb-base',
+      'plugin:jest/all',
+    ],
+    globals: {
+      Atomics: 'readonly',
+      SharedArrayBuffer: 'readonly',
+    },
+    parserOptions: {
+      ecmaVersion: 2018,
+      sourceType: 'module',
+    },
+    plugins: ['jest'],
+    rules: {
+      "jest/no-disabled-tests": "warn",
+      "jest/no-focused-tests": "error",
+      "jest/no-identical-title": "error",
+      "jest/prefer-to-have-length": "warn",
+      "jest/valid-expect": "error",
+      'no-console': 'off',
+      'no-shadow': 'off',
+      'no-restricted-syntax': [
+        'error',
+        'LabeledStatement',
+        'WithStatement',
+      ],
+    },
+    overrides:[
+      {
+        files: ['*.js'],
+        excludedFiles: 'babel.config.js',
+      }
+    ]
+};
