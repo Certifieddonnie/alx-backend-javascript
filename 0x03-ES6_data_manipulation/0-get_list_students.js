bf8342274ae37988e3ed3a02e37845e8d2844d8a
Certifieddonnie <mcdonaldsamure91@gmail.com>
2023-04-24 14:12:46 +0100

    [Task 2]: More mapping

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,7 +1,10 @@
 export default function getListStudents() {
-  const student1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
-  const student2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const student3 = { id: 5, firstName: 'Serena', location: 'San Francisco' };
+  const student1 = new Set();
+  const student2 = new Set();
+  const student3 = new Set();
+  student1.add({ id: 1, firstName: 'Guillaume', location: 'San Francisco' });
+  student2.add({ id: 2, firstName: 'James', location: 'Columbia' });
+  student3.add({ id: 5, firstName: 'Serena', location: 'San Francisco' });
 
-  return [student1, student2, student3];
+  return [...student1, ...student2, ...student3];
 }
